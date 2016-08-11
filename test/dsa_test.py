5371e7f31682989bf24cc72e63ee7ec9311651cd
Tom Barber <thermalbarker@gmail.com>
2016-08-11 07:46:30 +0200

    Completed Challenge 44

--- test/dsa_test.py
+++ test/dsa_test.py
@@ -1,5 +1,6 @@
 import unittest
 import random
+import itertools
 from dsa import dsa
 import cryptomath
 from cryptobuffer import cryptobuffer
@@ -11,6 +12,9 @@ class dsa_test(unittest.TestCase):
     y = 0x84ad4719d044495496a3201c8ff484feb45b962e7302e56a392aee4abab3e4bdebf2955b4736012f21a08084056b19bcd7fee56048e004e44984e2f411788efdc837a0d2e5abb7b555039fd243ac01f0fb2ed1dec568280ce678e931868d23eb095fde9d3779191b8c0299d6e07bbb283e6633451e535c45513b2d33c99ea17
     r = 548099063082341131477253921760299949438196259240
     s = 857042759984254168557880549501802188789837994940
+
+    # Public key from challenge 44
+    y44 = 0x2d026f4bf30195ede3a088da85e398ef869611d0f68f0713d51c9c1a3a26c95105d915e2d8cdf26d056b86b8a7b85519b1c23cc3ecdc6062650462e3063bd179c2a6581519f674a61f1d89a1fff27171ebc1b93d4dc57bceb7ae2430f98a6a4d83d8279ee65d71c1203d2c96d65ebbf7cce9d32971c3de5084cce04a2e147821
 
     def setUp(self):
         random.seed()
@@ -35,6 +39,13 @@ class dsa_test(unittest.TestCase):
         r, s = d.sign(x, self.m)
         self.assertTrue(d.verify(y, self.m, r, s))
         
+    def private_key_hash(self, x):
+        d = dsa()
+        # Now check the hash of the private key
+        x_buf = cryptobuffer()
+        x_buf.fromInt(x)
+        return d.hash(x_buf.toHex())        
+
     def test_key_recovery(self):
         d = dsa()
         
@@ -51,13 +62,8 @@ class dsa_test(unittest.TestCase):
                 break
 
         self.assertTrue(success)
-
-        # Now check the hash of the private key
-        x_buf = cryptobuffer()
-        x_buf.fromInt(x)
-        x_hash = d.hash(x_buf.toHex())
+        x_hash = self.private_key_hash(x)
         self.assertEqual(0x0954edd5e0afe5542a4adf012611a91912a3ec16, x_hash)
-
 
     def load_signatures(self, filename):
         signatures = []
@@ -69,12 +75,30 @@ class dsa_test(unittest.TestCase):
             r = int(lines[i * 4 + 2])
             m = int(lines[i * 4 + 3], 16)
             signatures.append( (msg, s, r, m) )
-        print signatures
         return signatures
-        
 
     def test_repeated_nonce(self):
-        self.load_signatures("data/44.txt")
+        d = dsa()
+        success = False
+        sigs = self.load_signatures("data/44.txt")
+        # Try each pair of lines
+        for pair in itertools.permutations(sigs, 2):
+            m1 = pair[0][0]
+            m2 = pair[1][0]
+            s1 = pair[0][1]
+            s2 = pair[1][1]
+            r1 = pair[0][2]
+            r2 = pair[1][2]
+            k = d.recover_k(m1, m2, s1, s2)
+            x = d.recover_x(k, m1, r1, s1)
+            y = d.get_pub_key(x)
+            if y == self.y44:
+                success = True
+                break
+
+        self.assertTrue(success)
+        x_hash = self.private_key_hash(x)
+        self.assertEqual(0xca8f6f7c66fa362d40760d135b763eb8527d3d52, x_hash)
 
 
 if __name__ == '__main__':
